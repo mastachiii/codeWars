0eef6789d2cc290d3092919222485d6a12ab5456
mastachiii <mastachii273@gmail.com>
2024-05-09 15:42:07 +0800

    Add 8th Kyu Exercises 1 - 3

---
+++ 8Kyu/2.Find the Difference in Age between Oldest and Youngest Family Members/script.js
@@ -0,0 +1,22 @@
+/*
+
+START;
+
+1. Create a function
+2. Sort out the given array and put the youngest age & the oldest age in a new array.
+3. Add a new string in the array containing the difference in age between the youngest and oldest.
+4. Return the result.
+
+;END */
+
+
+function ageDiff(arr) {
+  a = Math.min(...arr);
+  b = Math.max(...arr);
+  c = abs([a, b, a - b]);
+  return c;
+}
+
+test = ageDiff([12,34,56,20])
+
+console.log(test);
